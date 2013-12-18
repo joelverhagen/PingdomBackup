2e851e5389cab1a56dbae7bf8964cc87f96ad43d
Joel Verhagen <joel.verhagen@gmail.com>
2013-12-18 02:41:48 -0500

    Fix blasted quotes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pingdombackup',
-    version="0.1.0",
+    version='0.1.0',
     description='Backup Pingdom logs',
     long_description='Backup Pingdom result logs to a SQLite database.',
     author='Joel Verhagen',
