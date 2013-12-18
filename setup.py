bc17ea522b0120ec7308ba0309d87b18ba9163d9
Joel Verhagen <joel.verhagen@gmail.com>
2013-12-18 02:40:14 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+from setuptools import setup
+
+setup(
+    name='pingdombackup',
+    version="0.1.0",
+    description='Backup Pingdom logs',
+    long_description='Backup Pingdom result logs to a SQLite database.',
+    author='Joel Verhagen',
+    author_email='joel.verhagen@gmail.com',
+    install_requires=['requests>=2.1.0'],
+    url='https://github.com/joelverhagen/PingdomBackup',
+    packages=['pingdombackup'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Monitoring'
+    ]
+)
